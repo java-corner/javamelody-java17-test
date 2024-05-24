1f582769d783d8198ef6b909be363395da7cfc32
Jia Cheng Teo <jcteo.2018@scis.smu.edu.sg>
2024-05-24 13:34:17 +0800

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+	public static void main(String[] args) {
+		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+		// to see how IntelliJ IDEA suggests fixing it.
+		System.out.printf("Hello and welcome!");
+
+		// Generate a code that uses Java Melody to monitor the application
+		//TIP Press <shortcut actionId="Generate"/> with your caret at the highlighted text
+		// to see how IntelliJ IDEA suggests generating code.
+		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+		// to see how IntelliJ IDEA suggests fixing it.
+
+		// Generate a code that uses Java Melody to monitor the application
+		//TIP Press <shortcut actionId="Generate"/> with your caret at the highlighted text
+
+	}
+}
