d5e13cbbdded17c83bc6f5afddf03d1af52b078a
Jia Cheng Teo <jcteo.2018@scis.smu.edu.sg>
2024-05-24 13:42:20 +0800

    add jakarta persistence api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import jakarta.persistence.spi.PersistenceProvider;
+import net.bull.javamelody.JpaPersistence;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,15 +10,6 @@ public class Main {
 		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 		// to see how IntelliJ IDEA suggests fixing it.
 		System.out.printf("Hello and welcome!");
-
-		// Generate a code that uses Java Melody to monitor the application
-		//TIP Press <shortcut actionId="Generate"/> with your caret at the highlighted text
-		// to see how IntelliJ IDEA suggests generating code.
-		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-		// to see how IntelliJ IDEA suggests fixing it.
-
-		// Generate a code that uses Java Melody to monitor the application
-		//TIP Press <shortcut actionId="Generate"/> with your caret at the highlighted text
-
+		JpaPersistence jpaPersistence = new JpaPersistence();
 	}
 }
